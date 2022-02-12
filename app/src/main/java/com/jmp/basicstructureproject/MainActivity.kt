3eba01d5f6257cea3b5b4d49377a341508c99bcc
junemyounglee <june5215@naver.com>
2022-02-12 11:57:07 +0900

    1. viewBinding 추가 2. MainFragment 연결

--- app/src/main/java/com/jmp/basicstructureproject/MainActivity.kt
+++ app/src/main/java/com/jmp/basicstructureproject/MainActivity.kt
@@ -1,16 +1,42 @@
 package com.jmp.basicstructureproject
 
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
+import com.jmp.basicstructureproject.databinding.ActivityMainBinding
+import com.jmp.basicstructureproject.presentation.MainFragment
 
 class MainActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         (application as AppApplication).appComponent.inject(this)
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var bundle: Bundle? = null
 
-        Toast.makeText(applicationContext, "앱 실행", Toast.LENGTH_SHORT).show()
+        if (savedInstanceState == null) {
+            if(!MainFragment.newInstance(bundle = bundle).isAdded){
+                supportFragmentManager.beginTransaction()
+                    .replace(
+                        R.id.mainFragmentHolder,
+                        MainFragment.newInstance(bundle = bundle)
+                    )
+                    .commitNow()
+
+                window.run {
+                    clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+                    statusBarColor = 0x00000000  // transparent
+                    addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                    decorView.systemUiVisibility =
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/jmp/basicstructureproject/presentation/MainFragment.kt
+++ app/src/main/java/com/jmp/basicstructureproject/presentation/MainFragment.kt
@@ -3,15 +3,21 @@ package com.jmp.basicstructureproject.presentation
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.jmp.basicstructureproject.AppApplication
+import com.jmp.basicstructureproject.R
+import com.jmp.basicstructureproject.databinding.FragmentMainBinding
+import com.jmp.basicstructureproject.presentation.extension.viewBinding
 
-class MainFragment: Fragment() {
+class MainFragment: Fragment(R.layout.fragment_main) {
+
+    private val binding by viewBinding (FragmentMainBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        Toast.makeText(requireContext(), "Welcome to MainFragment", Toast.LENGTH_SHORT).show()
     }
 
     override fun onAttach(context: Context) {
@@ -22,5 +28,14 @@ class MainFragment: Fragment() {
             .inject(this)
 
         super.onAttach(context)
+    }
+
+    companion object {
+        fun newInstance(bundle: Bundle?): MainFragment {
+            schemeBundle = bundle
+            return MainFragment()
+        }
+
+        private var schemeBundle: Bundle? = null
     }
 }
